9704e68bc40fb422e71ae19aef0ca4856657e5d7
Imashev Olzhas <imashev.olzhas@gmail.com>
2023-11-24 22:14:48 +0600

    added chapter 18

--- assignment_2/greenlight/cmd/api/main.go
+++ assignment_2/greenlight/cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql" 
 	"flag"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -37,7 +38,10 @@ type config struct {
 		username string
 		password string
 		sender string
-	}		
+	}
+	cors struct {
+		trustedOrigins []string
+	}
 }
 
 type application struct {
@@ -69,6 +73,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "62fc01dbea4043", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "f863c5b8710c24", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.alexedwards.net>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
